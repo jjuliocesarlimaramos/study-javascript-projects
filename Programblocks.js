89a52f195fdcf506be21f48e33114f37da9f51b4
unknown <user_arpia153@tribanco.com.br	~>
2025-07-24 10:42:04 -0300

    js-study

---
+++ Programblocks.js
@@ -0,0 +1,8 @@
+let  counter;
+console.log(counter);  //  ->  undefined
+{
+         counter  =  1;
+         console.log(counter);  //  ->  1
+}
+counter  =  counter  +  1;
+console.log(counter);  //  ->  2
